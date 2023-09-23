489f88fb884604ce96b4d22bc16896796e119a98
Hardik Jain <jainhardik120@gmail.com>
2023-09-23 14:22:10 +0530

    Completed Auth Router

--- database.sql
+++ database.sql
@@ -1,6 +1,28 @@
 CREATE DATABASE greenovate;
 
-CREATE TABLE companies(
-    company_id SERIAL PRIMARY KEY,
-    company_name VARCHAR(255)
+CREATE TABLE organization (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Factor (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE SubFactor (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    factor_id INT NOT NULL,
+    FOREIGN KEY (factor_id) REFERENCES Factor(id)
+);
+
+CREATE TABLE SubSubFactor (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    sub_factor_id INT NOT NULL,
+    emission_factor FLOAT NOT NULL,
+    FOREIGN KEY (sub_factor_id) REFERENCES SubFactor(id)
 );
--- index.js
+++ index.js
@@ -10,35 +10,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post("/company", async (req, res) => {
-    try {
-        const { name } = req.body;
-        const newCompany = await pool.query("INSERT INTO companies (company_name) VALUES ($1) RETURNING *", [name]);
-        return res.json(newCompany.rows[0]);
-    } catch (error) {
-        console.log(error.message);
-    }
-})
+const authRouter = require("./routes/auth.routes");
 
-app.get("/companies", async (req, res)=>{
-    try {
-        const all = await pool.query("SELECT * FROM companies");
-        return res.json(all.rows);
-    } catch (error) {
-        console.log(error.message);
-    }
-})
-
-app.get("/company/:id", async(req, res)=>{
-    try {
-        const id = req.params.id;
-        const row = await pool.query("SELECT * FROM companies WHERE company_id = $1", [id]);
-        return res.json(row.rows[0]);
-    } catch (error) {
-        console.log(error.message);
-        return res.json({error : error.message});
-    }
-})
+app.use("/api/auth", authRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
